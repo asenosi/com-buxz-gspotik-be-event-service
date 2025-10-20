4ef09ca07a73918b5f67f2a92b1e7a06081e4046
Aobakwe Senosi <senosiag@gmail.com>
2025-10-20 17:11:48 +0200

    Just commit for now

---
+++ src/test/java/com/buxz/gspotik/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+//package com.buxz.gspotik;
+//
+//import io.quarkus.test.junit.QuarkusIntegrationTest;
+//
+//@QuarkusIntegrationTest
+//class GreetingResourceIT extends GreetingResourceTest {
+//    // Execute the same tests but in packaged mode.
+//}
